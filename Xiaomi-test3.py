39af564dc40f58095203cc034395064ad076b109
Deyvidleo <134196708+Deyvidleo@users.noreply.github.com>
2023-07-01 15:08:14 +0700

    Create Xiaomi-test3.py

---
+++ Xiaomi-test3.py
@@ -0,0 +1,6 @@
+shell_command:
+  update_xiaomi_miot: |-
+    wget -q -O - https://raw.githubusercontent.com/al-one/hass-xiaomi-miot/master/install.sh | ARCHIVE_TAG=latest bash -
+Verify
+Do not know
+Game end
